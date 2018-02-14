f08adee65c112f3148165385fe4d53179649bc2d
wktdev <wktdev@gmail.com>
2018-02-14 02:22:43 -0800

    complete v1  checkpoint 3 list-messages

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -3,40 +3,65 @@ import React, { Component } from 'react';
 
 class MessageList extends Component {
 
-	constructor(props){
-		super(props)
+    constructor(props) {
+        super(props)
 
-		this.state = {
-          messages:[]
+        this.state = {
+            messages: []
 
-		}
+        }
 
-		this.messagesRef = this.props.firebase.database().ref('messages')
-
-
-
-	}
-
-	componentDidMount() {
-    this.messagesRef.on('child_added', snapshot => {
-        
-    
-
-    	console.log(snapshot.val());
-
-
-        
-        
-     });
- 
-  }
+        this.messagesRef = this.props.firebase.database().ref('messages')
 
 
 
-	render(){
-		console.log(this.messagesRef);
-		return <div> <b> Active room key: {this.props.activeRoom} </b></div>
-	}
+    }
+
+    componentDidMount() {
+        let temp = [];
+        this.messagesRef.on('child_added', snapshot => {
+            console.log(snapshot.val());
+
+            temp.push(snapshot.val())
+
+            this.setState({
+                messages: temp
+            })
+
+            console.log(this.state.messages);
+
+        });
+
+    }
+
+
+
+    render() {
+
+
+        let result = this.state.messages.map((val, index) => {
+            console.log(val.roomId);
+
+            if (this.props.activeRoom === val.roomId) {
+                return <li key={index}>{val.content}</li>
+            }else{
+            	return undefined
+            }
+        })
+
+        return (
+            <div>
+              <ul>
+              {result}
+              </ul>
+
+            </div>
+
+        )
+
+
+
+    }
 }
 
 export default MessageList
